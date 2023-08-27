40575b1bb2703b34f1db5604d8da3d7f2e23eabb
SpandanBG <spandanbg@gmail.com>
2023-08-27 20:53:33 +0530

    add simplex solver logic

--- src/coord.rs
+++ src/coord.rs
@@ -14,7 +14,7 @@
 //! assert_eq!(coord.y, 1);
 //! ```
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 /// Represents the coordinate in matrix of a fixed size
 pub struct Coord {
     /// The x axis position
@@ -30,41 +30,33 @@ pub struct Coord {
     width: usize,
 }
 
-impl Coord {
-    /// Creates a new coord out of an array index for fixed width and height
-    ///
-    /// # Arguments
-    ///
-    /// * `idx` - `usize` the array index
-    /// * `width` - `usize` the width of the matrix
-    /// * `height` - `usize` the height of the matrix
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simplex_method_lp::coord::Coord;
-    ///
-    /// // In a 2x2 matrix, in a single 4 length array, the array index 3 will be
-    /// // coord (1, 1):
-    /// let coord = Coord::new(3, 2, 2);
-    /// ```
-    pub fn new(idx: usize, width: usize, height: usize) -> Coord {
-        let x = idx % width;
-        let y = idx / width;
-        return Coord {
-            x,
-            y,
-            height,
-            width,
-        };
-    }
+#[derive(Debug)]
+/// Possible coordinate navigation direction
+pub enum NavDir {
+    /// Move y = y - 1
+    UP,
 
+    /// Move y = y + 1
+    DOWN,
+
+    /// Move x = x - 1
+    LEFT,
+
+    /// Move x = x + 1
+    RIGHT,
+}
+
+#[derive(Debug)]
+/// Represents the iterator of a Coord to the direction given by NavDir
+pub struct CoordIter(Option<Coord>, NavDir);
+
+impl Coord {
     /// Creates a new coord with x and y directly
     ///
     /// # Arguments
     ///
-    /// * `x` - `usize` the x axis location 
-    /// * `y` - `usize` the y axis location 
+    /// * `x` - `usize` the x axis location
+    /// * `y` - `usize` the y axis location
     /// * `width` - `usize` the width of the matrix
     /// * `height` - `usize` the height of the matrix
     ///
@@ -79,129 +71,13 @@ impl Coord {
     /// assert_eq!(coord.x, 1);
     /// assert_eq!(coord.y, 1);
     /// ```
-    pub fn new_raw(x: usize, y: usize, width: usize, height: usize) -> Coord {
+    pub fn new(x: usize, y: usize, width: usize, height: usize) -> Coord {
         return Coord {
             x,
             y,
             width,
             height,
         };
-    }
-
-    /// Get the coordinate above the current one
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simplex_method_lp::coord::Coord;
-    ///
-    /// // In a 2x2 matrix: coord (1, 1);
-    /// let coord = Coord::new_raw(1, 1, 2, 2);
-    ///
-    /// // up coordinate will be: coord (1, 0);
-    /// let up = coord.up().unwrap();
-    ///
-    /// assert_eq!(up.x, 1);
-    /// assert_eq!(up.y, 0);
-    /// ```
-    pub fn up(&self) -> Option<Self> {
-        if self.y == 0 {
-            return None;
-        }
-
-        return Some(Self {
-            x: self.x,
-            y: self.y - 1,
-            width: self.width,
-            height: self.height,
-        });
-    }
-
-    /// Get the coordinate below the current one
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simplex_method_lp::coord::Coord;
-    ///
-    /// // In a 2x2 matrix: coord (0, 0);
-    /// let coord = Coord::new_raw(0, 0, 2, 2);
-    ///
-    /// // up coordinate will be: coord (0, 1);
-    /// let down = coord.down().unwrap();
-    ///
-    /// assert_eq!(down.x, 0);
-    /// assert_eq!(down.y, 1);
-    /// ```
-    pub fn down(&self) -> Option<Self> {
-        if self.y == self.height - 1 {
-            return None;
-        }
-
-        return Some(Self {
-            x: self.x,
-            y: self.y + 1,
-            width: self.width,
-            height: self.height,
-        });
-    }
-  
-    /// Get the coordinate left the current one
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simplex_method_lp::coord::Coord;
-    ///
-    /// // In a 2x2 matrix: coord (1, 1);
-    /// let coord = Coord::new_raw(1, 1, 2, 2);
-    ///
-    /// // up coordinate will be: coord (0, 1);
-    /// let left = coord.left().unwrap();
-    ///
-    /// assert_eq!(left.x, 0);
-    /// assert_eq!(left.y, 1);
-    /// ```
-    pub fn left(&self) -> Option<Self> {
-        if self.x == 0 {
-            return None;
-        }
-
-        return Some(Self {
-            x: self.x - 1,
-            y: self.y,
-            width: self.width,
-            height: self.height,
-        });
-    }
-
-    /// Get the coordinate right the current one
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simplex_method_lp::coord::Coord;
-    ///
-    /// // In a 2x2 matrix: coord (0, 0);
-    /// let coord = Coord::new_raw(0, 0, 2, 2);
-    ///
-    /// // up coordinate will be: coord (1, 0);
-    /// let right = coord.right().unwrap();
-    ///
-    /// assert_eq!(right.x, 1);
-    /// assert_eq!(right.y, 0);
-    /// ```
-    pub fn right(&self) -> Option<Self> {
-        if self.x == self.width - 1 {
-            return None;
-        }
-
-        Some(Self {
-            x: self.x + 1,
-            y: self.y,
-            width: self.width,
-            height: self.height,
-        })
     }
 
     /// Get the coordinate right the current one
@@ -218,7 +94,100 @@ impl Coord {
     /// assert_eq!(idx, 2);
     /// ```
     pub fn to_index(&self) -> usize {
-        self.y * self.width + self.x
+        return self.y * self.width + self.x;
+    }
+
+    /// Create a coordinate iterator
+    ///
+    /// # Arguments
+    /// * `dir` - `NavDir` The direction the coordinate to iterate towards
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use simplex_method_lp::coord::Coord;
+    ///
+    /// // A (1, 1) coordinate in a 2x2 matrix
+    /// let coord = Coord::new_raw(1, 1, 2, 2);
+    ///
+    /// let nav_up_iter = coord.iter(NavDir::UP);
+    /// let next_up = nav_up_iter.next();
+    ///
+    /// assert_eq!(next_up.x, 1);
+    /// assert_eq!(next_up.y, 0);
+    /// ```
+    pub fn iter(&self, dir: NavDir) -> CoordIter {
+        return CoordIter(Some(self.clone()), dir);
+    }
+
+    fn up(&self) -> Option<Self> {
+        if self.y == 0 {
+            return None;
+        }
+
+        return Some(Self {
+            x: self.x,
+            y: self.y - 1,
+            width: self.width,
+            height: self.height,
+        });
+    }
+
+    fn down(&self) -> Option<Self> {
+        if self.y == self.height - 1 {
+            return None;
+        }
+
+        return Some(Self {
+            x: self.x,
+            y: self.y + 1,
+            width: self.width,
+            height: self.height,
+        });
+    }
+
+    fn left(&self) -> Option<Self> {
+        if self.x == 0 {
+            return None;
+        }
+
+        return Some(Self {
+            x: self.x - 1,
+            y: self.y,
+            width: self.width,
+            height: self.height,
+        });
+    }
+
+    fn right(&self) -> Option<Self> {
+        if self.x == self.width - 1 {
+            return None;
+        }
+
+        Some(Self {
+            x: self.x + 1,
+            y: self.y,
+            width: self.width,
+            height: self.height,
+        })
+    }
+}
+
+impl Iterator for CoordIter {
+    type Item = Coord;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.is_none() {
+            return None;
+        }
+        let curr = self.0.clone();
+        self.0 = match &self.1 {
+            NavDir::UP => self.0.as_ref().unwrap().up(),
+            NavDir::DOWN => self.0.as_ref().unwrap().down(),
+            NavDir::LEFT => self.0.as_ref().unwrap().left(),
+            NavDir::RIGHT => self.0.as_ref().unwrap().right(),
+        };
+        return curr;
     }
 }
 
@@ -226,117 +195,99 @@ impl Coord {
 mod coord_tests {
     use super::Coord;
 
-    fn setup_coord(idx: usize) -> Coord {
-        // in a 2x2 matrix represented by an array of size 4 
-        // then, the index 2 is can be represented by (0, 1);
-        return Coord::new(idx, 2, 2);
-    }
-
     #[test]
-    fn should_create_raw_coords() {
-        let coord = Coord::new_raw(1, 1, 2, 2);
+    fn should_create_coords() {
+        let coord = Coord::new(1, 1, 2, 2);
 
         assert_eq!(coord.x, 1);
         assert_eq!(coord.y, 1);
     }
-
-    #[test]
-    fn should_return_coords_for_index() {
-        let coord = setup_coord(2);
-
-        assert_eq!(coord.x, 0);
-        assert_eq!(coord.y, 1);
-    }
-
-    #[test]
-    fn should_return_none_for_up_coord() {
-        let coord = setup_coord(0);
-
-        let up = coord.up();
-
-        assert!(up.is_none());
-    }
-
-
-    #[test]
-    fn should_return_0_0_for_up_coord() {
-        let coord = setup_coord(2);
-
-        let up = coord.up();
-        let up = up.unwrap();
-
-        assert_eq!(up.x, 0);
-        assert_eq!(up.y, 0);
-    }
-
-    #[test]
-    fn should_return_none_for_left_coord() {
-        let coord = setup_coord(0);
-
-        let left = coord.left();
-
-        assert!(left.is_none());
-    }
-
-    #[test]
-    fn should_return_0_0_for_left_coord() {
-        let coord = setup_coord(1);
-
-        let left = coord.left();
-        let left = left.unwrap();
-
-        assert_eq!(left.x, 0);
-        assert_eq!(left.y, 0);
-    }
-
-    #[test]
-    fn should_return_none_for_right_coord() {
-        let coord = setup_coord(1);
-
-        let right = coord.right();
-
-        assert!(right.is_none());
-    }
-
-    #[test]
-    fn should_return_0_1_for_right_coord() {
-        let coord = setup_coord(0);
-
-        let right = coord.right();
-        let right = right.unwrap();
-
-        assert_eq!(right.x, 1);
-        assert_eq!(right.y, 0);
-    }
-
-
-    #[test]
-    fn should_return_none_for_down_coord() {
-        let coord = setup_coord(2);
-
-        let down = coord.down();
-
-        assert!(down.is_none());
-    }
-
-    #[test]
-    fn should_return_0_1_for_down_coord() {
-        let coord = setup_coord(0);
-
-        let down = coord.down();
-        let down = down.unwrap();
-
-        assert_eq!(down.x, 0);
-        assert_eq!(down.y, 1);
-    }
-
-    #[test]
-    fn should_return_index_from_coord() {
-        let coord = setup_coord(2);
-
-        let idx = coord.to_index();
-
-        assert_eq!(idx, 2);
-    }
 }
 
+#[cfg(test)]
+mod coord_iter_tests {
+    use super::{Coord, NavDir};
+
+    fn setup_coord() -> Coord {
+        // A 9x9 Matrix with coord at (4, 4);
+        return Coord::new(4, 4, 9, 9);
+    }
+
+    #[test]
+    fn should_iterate_to_top() {
+        let coord = setup_coord();
+        let mut no_of_asserts = 0;
+
+        let up_iter = coord.iter(NavDir::UP);
+
+        for (i, next_up_coord) in up_iter.enumerate() {
+            assert_eq!(next_up_coord.x, 4);
+            assert_eq!(next_up_coord.y, 4 - i);
+            no_of_asserts += 1;
+        }
+        assert_eq!(no_of_asserts, 5);
+    }
+
+    #[test]
+    fn should_iterate_to_bottom() {
+        let coord = setup_coord();
+        let mut no_of_asserts = 0;
+
+        let up_iter = coord.iter(NavDir::DOWN);
+
+        for (i, next_up_coord) in up_iter.enumerate() {
+            assert_eq!(next_up_coord.x, 4);
+            assert_eq!(next_up_coord.y, 4 + i);
+            no_of_asserts += 1;
+        }
+        assert_eq!(no_of_asserts, 5);
+    }
+
+    #[test]
+    fn should_iterate_to_left() {
+        let coord = setup_coord();
+        let mut no_of_asserts = 0;
+
+        let up_iter = coord.iter(NavDir::LEFT);
+
+        for (i, next_up_coord) in up_iter.enumerate() {
+            assert_eq!(next_up_coord.x, 4 - i);
+            assert_eq!(next_up_coord.y, 4);
+            no_of_asserts += 1;
+        }
+        assert_eq!(no_of_asserts, 5);
+    }
+
+    #[test]
+    fn should_iterate_to_right() {
+        let coord = setup_coord();
+        let mut no_of_asserts = 0;
+
+        let up_iter = coord.iter(NavDir::RIGHT);
+
+        for (i, next_up_coord) in up_iter.enumerate() {
+            assert_eq!(next_up_coord.x, 4 + i);
+            assert_eq!(next_up_coord.y, 4);
+            no_of_asserts += 1;
+        }
+        assert_eq!(no_of_asserts, 5);
+    }
+
+    #[test]
+    fn should_iterate_top_to_bottom_from_left_to_right() {
+        // (0, 0) for a 10x10 matrix
+        let coord = Coord::new(0, 0, 10, 10);
+
+        let mut no_of_assertions = 0;
+
+        for (i, rt_coord) in coord.iter(NavDir::RIGHT).enumerate() {
+            for (j, dwn_coord) in rt_coord.iter(NavDir::DOWN).enumerate() {
+                assert_eq!(dwn_coord.x, i);
+                assert_eq!(dwn_coord.y, j);
+                no_of_assertions += 1;
+            }
+        }
+
+        assert_eq!(no_of_assertions, 100);
+    }
+}
--- src/simplex.rs
+++ src/simplex.rs
@@ -1,22 +1,37 @@
 //! This feature module contains the algorithm to solve a Maximization and Minimization problem
 //! using the Simplex method.
 
-use crate::equation::Equation;
+use crate::{
+    coord::{Coord, NavDir},
+    equation::Equation,
+};
 
 #[derive(Debug, PartialEq)]
 /// Possible problem type
 pub enum ProblemType {
+    /// Maximization problem
     MAX = 1,
+
+    /// Minimization problem
     MIN = 2,
 }
 
 #[derive(Debug)]
 /// Represents the simplex table in a single iteration
 pub struct SimplexTable {
+    /// Iteration number while solving the simplex table
     iteration_no: u64,
+
+    /// Problem type (maximization / minimization problem)
     problem_type: ProblemType,
+
+    /// Cj => Coeff.s of Objective fn (`aM`)
     c_j: Vec<f64>,
+
+    /// Basic Variables => Variales of Objective fn (`xM`)
     basic_var: Vec<u64>,
+
+    /// 
     c_b_i: Vec<f64>,
     entered_basic_var: Vec<u64>,
     solution: Vec<f64>,
@@ -24,6 +39,9 @@ pub struct SimplexTable {
     z_j: Vec<f64>,
     c_j_minus_z_j: Vec<f64>,
     st_table: Vec<f64>,
+    st_key_col: Vec<f64>,
+    st_key_row: Vec<f64>,
+    st_key_elem: f64,
     st_height: usize,
     st_width: usize,
 }
@@ -37,7 +55,7 @@ impl SimplexTable {
         let (c_j, basic_var) = SimplexTable::get_cj_and_basic_vars(objective_fn);
         let (c_b_i, entered_basic_var) =
             SimplexTable::get_cbi_and_entered_basic_vars(&c_j, &basic_var, conditions.len());
-        let (solution, ratio) = SimplexTable::get_sols_and_ratios(&conditions);
+        let solution = SimplexTable::get_sols(&conditions);
         let (st_table, st_height, st_width) = SimplexTable::get_soluton_table(&conditions);
 
         SimplexTable {
@@ -48,10 +66,13 @@ impl SimplexTable {
             c_b_i,
             entered_basic_var,
             solution,
-            ratio,
+            ratio: vec![0e0; conditions.len()],
             z_j: vec![0e0; st_width],
             c_j_minus_z_j: vec![0e0; st_width],
             st_table,
+            st_key_col: vec![0e0; st_height],
+            st_key_row: vec![0e0; st_width + 1],
+            st_key_elem: 0e0,
             st_height,
             st_width,
         }
@@ -88,16 +109,12 @@ impl SimplexTable {
         return (c_b_i, entered_basic_var);
     }
 
-    fn get_sols_and_ratios(conditions: &Vec<Equation>) -> (Vec<f64>, Vec<f64>) {
+    fn get_sols(conditions: &Vec<Equation>) -> Vec<f64> {
         let mut solution = vec![];
-        let mut ratio = vec![];
-
         for cond in conditions.iter() {
             solution.push(cond.get_rhs());
-            ratio.push(cond.get_rhs());
         }
-
-        return (solution, ratio);
+        return solution;
     }
 
     fn get_soluton_table(conditions: &Vec<Equation>) -> (Vec<f64>, usize, usize) {
@@ -109,12 +126,136 @@ impl SimplexTable {
         let st_width = st_table.len() / st_height;
         return (st_table, st_height, st_width);
     }
+
+    pub fn next(&mut self) -> bool {
+        self.update_z_j();
+        let key_col = self.update_c_j_minus_z_j();
+
+        if self.is_solved() {
+            return false;
+        }
+
+        let key_row = self.update_ratio(key_col);
+        self.update_st_key_elem(key_col, key_row);
+        self.update_st_key_row(key_row);
+        self.update_st_rest(key_row);
+        self.update_entries(key_col, key_row);
+        self.iteration_no += 1;
+
+        return true;
+    }
+
+    fn st_coord(&self, x: usize, y: usize) -> Coord {
+        return Coord::new(x, y, self.st_width, self.st_height);
+    }
+
+    fn is_solved(&self) -> bool {
+        let optimality_fn = match self.problem_type {
+            ProblemType::MAX => |&each: &f64| each <= 0e0,
+            ProblemType::MIN => |&each: &f64| each >= 0e0,
+        };
+        return self.c_j_minus_z_j.iter().all(optimality_fn);
+    }
+
+    fn update_z_j(&mut self) {
+        let st_table_coord = self.st_coord(0, 0);
+        for (j, rt_coord) in st_table_coord.iter(NavDir::RIGHT).enumerate() {
+            let mut z_j_i = 0e0;
+            for (i, dwn_coord) in rt_coord.iter(NavDir::DOWN).enumerate() {
+                z_j_i += self.st_table[dwn_coord.to_index()] * self.c_b_i[i];
+            }
+            self.z_j[j] = z_j_i;
+        }
+    }
+
+    fn update_c_j_minus_z_j(&mut self) -> usize {
+        let mut largest_value = 0e0;
+        let mut key_col = 0;
+        for i in 0..self.z_j.len() {
+            let updated_val = self.c_j[i] - self.z_j[i];
+            if updated_val > largest_value {
+                largest_value = updated_val;
+                key_col = i;
+            }
+            self.c_j_minus_z_j[i] = updated_val;
+        }
+        for (i, key_coord) in self.st_coord(key_col, 0).iter(NavDir::DOWN).enumerate() {
+            self.st_key_col[i] = self.st_table[key_coord.to_index()];
+        }
+        return key_col;
+    }
+
+    fn update_ratio(&mut self, key_col: usize) -> usize {
+        let st_table_coord = self.st_coord(key_col, 0);
+        let mut smallest_value = f64::MAX;
+        let mut key_row = 0;
+        for (i, dwn_coord) in st_table_coord.iter(NavDir::DOWN).enumerate() {
+            let updated_value = self.solution[i] / self.st_table[dwn_coord.to_index()];
+            if updated_value < smallest_value {
+                smallest_value = updated_value;
+                key_row = i;
+            }
+            self.ratio[i] = updated_value;
+        }
+        for (i, key_coord) in self.st_coord(0, key_row).iter(NavDir::RIGHT).enumerate() {
+            self.st_key_row[i] = self.st_table[key_coord.to_index()];
+        }
+        let last_index = self.st_key_row.len() - 1;
+        self.st_key_row[last_index] = self.solution[key_row];
+        return key_row;
+    }
+
+    fn update_st_key_elem(&mut self, key_col: usize, key_row: usize) {
+        self.st_key_elem = self.st_table[self.st_coord(key_col, key_row).to_index()];
+    }
+
+    fn update_st_key_row(&mut self, key_row: usize) {
+        let row_coord = self.st_coord(0, key_row);
+        for rt_coord in row_coord.iter(NavDir::RIGHT) {
+            self.st_table[rt_coord.to_index()] /= self.st_key_elem;
+        }
+        self.solution[key_row] /= self.st_key_elem;
+    }
+
+    fn update_st_rest(&mut self, key_row: usize) {
+        for dwn_coord in self.st_coord(0, 0).iter(NavDir::DOWN) {
+            if dwn_coord.y == key_row {
+                continue;
+            }
+
+            for rt_coord in dwn_coord.iter(NavDir::RIGHT) {
+                let corres_key_col_val = self.st_key_col[rt_coord.y];
+                let corres_key_row_val = self.st_key_row[rt_coord.x];
+                self.st_table[rt_coord.to_index()] -=
+                    (corres_key_col_val * corres_key_row_val) / self.st_key_elem;
+            }
+        }
+        for i in 0..self.solution.len() {
+            if i == key_row {
+                continue;
+            }
+            let last_row_idx = self.st_key_row.len() - 1;
+            let corres_key_col_val = self.st_key_col[i];
+            let corres_key_row_val = self.st_key_row[last_row_idx];
+            self.solution[i] -= (corres_key_col_val * corres_key_row_val) / self.st_key_elem;
+        }
+    }
+
+    fn update_entries(&mut self, key_col: usize, key_row: usize) {
+        let new_entering_basic_var = self.basic_var[key_col];
+        let new_entering_c_b_i = self.c_j[key_col];
+        self.entered_basic_var[key_row] = new_entering_basic_var;
+        self.c_b_i[key_row] = new_entering_c_b_i;
+    }
 }
 
 #[cfg(test)]
 mod simplex_tests {
-    use crate::{equation::{Equation, DVar}, constants::EqType};
-    use super::{SimplexTable, ProblemType};
+    use super::{ProblemType, SimplexTable};
+    use crate::{
+        constants::EqType,
+        equation::{DVar, Equation},
+    };
 
     /// Setups the basic maximization problem
     ///
@@ -125,13 +266,25 @@ mod simplex_tests {
     /// - `x & y >= 0`
     fn setup_max_problem() -> (ProblemType, Equation, Vec<Equation>) {
         // `12x + 16y + 0s1 + 0s2`
-        let objective_fn = Equation::new(vec![DVar(12e0, 1), DVar(16e0, 2), DVar(0e0, 3), DVar(0e0, 4)], EqType::EQ, 0e0);
+        let objective_fn = Equation::new(
+            vec![DVar(12e0, 1), DVar(16e0, 2), DVar(0e0, 3), DVar(0e0, 4)],
+            EqType::EQ,
+            0e0,
+        );
 
         let subjected_to = vec![
-            // `10x + 12y + s1 + 0s2 + 0a1 + 0a2 <= 120`
-            Equation::new(vec![DVar(10e0, 1), DVar(12e0, 2), DVar(1e0, 3), DVar(0e0, 4)], EqType::LTE, 120e0),
+            // `10x + 20y + s1 + 0s2 + 0a1 + 0a2 <= 120`
+            Equation::new(
+                vec![DVar(10e0, 1), DVar(20e0, 2), DVar(1e0, 3), DVar(0e0, 4)],
+                EqType::LTE,
+                120e0,
+            ),
             // `8x + 8y + 0s1 + s2 + 0a1 + 0a2 <= 80`
-            Equation::new(vec![DVar(8e0, 1), DVar(8e0, 2), DVar(0e0, 3), DVar(1e0, 4)], EqType::LTE, 80e0),
+            Equation::new(
+                vec![DVar(8e0, 1), DVar(8e0, 2), DVar(0e0, 3), DVar(1e0, 4)],
+                EqType::LTE,
+                80e0,
+            ),
         ];
 
         return (ProblemType::MAX, objective_fn, subjected_to);
@@ -149,11 +302,32 @@ mod simplex_tests {
         assert_eq!(simplex.c_b_i, vec![0e0, 0e0]);
         assert_eq!(simplex.entered_basic_var, vec![3, 4]);
         assert_eq!(simplex.solution, vec![120e0, 80e0]);
-        assert_eq!(simplex.ratio, vec![120e0, 80e0]);
+        assert_eq!(simplex.ratio, vec![0e0, 0e0]);
         assert_eq!(simplex.z_j, vec![0e0, 0e0, 0e0, 0e0]);
         assert_eq!(simplex.c_j_minus_z_j, vec![0e0, 0e0, 0e0, 0e0]);
-        assert_eq!(simplex.st_table, vec![10e0, 12e0, 1e0, 0e0, 8e0, 8e0, 0e0, 1e0]);
+        assert_eq!(
+            simplex.st_table,
+            vec![10e0, 20e0, 1e0, 0e0, 8e0, 8e0, 0e0, 1e0]
+        );
         assert_eq!(simplex.st_height, 2);
         assert_eq!(simplex.st_width, 4);
+    }
+
+    #[test]
+    fn should_solve_simplex_table() {
+        let (problem_type, objective_fn, subjected_to) = setup_max_problem();
+        let mut simplex = SimplexTable::new(problem_type, &objective_fn, &subjected_to);
+
+        simplex.next();
+
+        assert_eq!(simplex.entered_basic_var, vec![2, 4]);
+        assert_eq!(simplex.solution, vec![6e0, 32e0]);
+
+        simplex.next();
+
+        assert_eq!(simplex.entered_basic_var, vec![2, 1]);
+        assert_eq!(simplex.solution, vec![2e0, 8e0]);
+
+        assert!(!simplex.next());
     }
 }
